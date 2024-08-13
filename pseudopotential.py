bdf4cb285c7d42014c837f41e947436c1a98a129
Max Parsons <mfpars@uw.edu>
2024-08-13 12:18:15 -0700

    added contour plot for potential

--- pseudopotential.py
+++ pseudopotential.py
@@ -76,7 +76,7 @@ class PseudopotentialPlanarTrap:
                                      ((self.a - x) ** 2 / y ** 2 + 1) * y ** 2))) / np.pi
         return grad_x, grad_y
 
-    def pseudo_ac(self, x, y):
+    def u_ac(self, x, y):
         """
         Returns pseudopotential from the AC electrodes, divided by the charge to mass ratio
         :param x:
@@ -86,7 +86,7 @@ class PseudopotentialPlanarTrap:
         gradx, grady = self.grad_phi(x, y)
         return (1. / (4 * self.omega ** 2)) * np.sqrt(gradx ** 2 + grady ** 2)
 
-    def phi_dc(self, x, y):
+    def u_dc(self, x, y):
         """
         Returns potential from the DC electrodes, divided by the charge to mass ratio
 
@@ -96,7 +96,7 @@ class PseudopotentialPlanarTrap:
         """
         return (self.v_dc / np.pi) * (np.arctan((self.x2 - x) / y) - np.arctan((self.x1 - x) / y))
 
-    def v_gravity(self, x, y):
+    def u_gravity(self, x, y):
         """
         Returns gravitational potential, divided by the charge to mass ratio
 
@@ -106,35 +106,51 @@ class PseudopotentialPlanarTrap:
         """
         return (1. / self.charge_to_mass) * g * y
 
-    def charToMassCalc(x, y, vDc):
-        raise NotImplementedError
-        return g / ((vDc * (-((x2 - x) * 1 / 1000) / (
-                    (((x2 - x) * 1 / 1000) ** 2 / (y * 1 / 1000) ** 2 + 1) * (y * 1 / 1000) ** 2) - (x * 1 / 1000) / (
-                                        (((x * 1 / 1000) / (y * 1 / 1000) - (x1 * 1 / 1000)) ** 2 + 1) * (
-                                            y * 1 / 1000) ** 2))) / Pi)
+    def u_total(self, x, y):
+        "Returns sum of gravitational, dc, and ac pseudopotential divided by charge to mass ratio"
+        return self.u_gravity(x, y) + self.u_dc(x, y) + self.u_ac(x, y)
 
-    def trapDepth(cm, vRf, a, b):
-        raise NotImplementedError
-        return (cm * vRf ** 2 / (Pi ** 2 * Omega ** 2)) * ((b * 0.001) / (
-                    (a * 0.001 + b * 0.001) ** 2 + (a * 0.001 + b * 0.001) * np.sqrt(
-                2 * a * b * 0.001 * 0.001 + (a * 0.001) ** 2))) ** 2
+    def plot_potential_contours(self, x_range=[0., 10.E-3], y_range=[0., 10.E-3], resolution=[512, 512],
+                                fig=None, ax=None):
+        x = np.linspace(x_range[0], x_range[1], num=resolution[0])
+        y = np.linspace(y_range[0], y_range[1], num=resolution[1])
+        x, y = np.meshgrid(x, y)
+        u = self.u_total(x, y)
+        extent = [x_range[0], x_range[1], y_range[0], y_range[1]]
+        if fig is None:
+            fig, ax = plt.subplots(1, 1)
+            levels = np.linspace(np.min(u.flatten()), np.max(u.flatten()), num=20)
+            # ax.contour(cs, colors='k')
+            cs = ax.imshow(np.flipud(u), extent=extent)
+            ax.contour(u, levels=15, colors='k', extent=extent)
+            ax.set_xlabel('x (mm)')
+            ax.set_ylabel('height (mm)')
+            ax.set_title('total trap potential energy (J / C / kg)')
+            fig.colorbar(cs)
+
 
 if __name__ == "__main__":
     p = PseudopotentialPlanarTrap()
-    y = np.linspace(0.E-3, 10.E-3, num=500)
-    x = np.zeros_like(y) + p.a / 2.
-
-    v_grav = p.v_gravity(x, y)
-    phi_dc = p.phi_dc(x, y)
-    psuedo = p.pseudo_ac(x, y)
-
-    fig, ax = plt.subplots(1, 1)
-    ax.plot(y * 1.E3, psuedo, label='psuedo')
-    ax.plot(y * 1.E3, phi_dc, label='dc')
-    ax.plot(y * 1.E3, v_grav, label='gravity')
-    ax.plot(y * 1.E3, v_grav + phi_dc + psuedo, label='total')
-    fig.legend()
-    ax.set_xlabel('y (mm)')
-    ax.grid()
-    ax.set_ylabel('potential (J / (coulomb / kg)')
+    p.v_dc = 0.
+    p.plot_potential_contours()
     plt.show()
+    # p.v_dc = 10.
+    # y = np.linspace(0.01E-3, 10.E-3, num=500)
+    # x = np.zeros_like(y) + p.a / 2.
+    #
+    # v_grav = p.u_gravity(x, y)
+    # phi_dc = p.u_dc(x, y)
+    # psuedo = p.u_ac(x, y)
+    #
+    # fig, ax = plt.subplots(1, 1)
+    # ax.plot(y * 1.E3, psuedo, label='psuedo')
+    # ax.plot(y * 1.E3, phi_dc, label='dc')
+    # ax.plot(y * 1.E3, v_grav, label='gravity')
+    # ax.plot(y * 1.E3, v_grav + phi_dc + psuedo, label='total')
+    # fig.legend()
+    # ax.set_xlabel('y (mm)')
+    # ax.grid()
+    # ax.set_ylabel('potential (J / (coulomb / kg)')
+    # plt.show()
+    #
+    # figim, axim = plt.subplots(1, 1)
