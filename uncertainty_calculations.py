223c6e56c3348c4db8a83114330669d34db878e4
cole0wolf <colewolf@uw.edu>
2025-02-04 10:15:19 -0800

    Updated voltage/height lists with uncertainty values

--- uncertainty_calculations.py
+++ uncertainty_calculations.py
@@ -1,10 +1,13 @@
 from micromotion_data_analysis import load_data, extract_data
 import os
+from uncertainties import ufloat
 
 class ParameterConfig:
     def __init__(self):
         self.input = "data/raw_micromotion"              # File or directory to be analyzed
         self.file_to_print = "8-18_Trial18_data.txt"     # Specific trial to print data from
+        self.dummy_height_error = 0.2                    # Dummy variable for the height value error
+        self.dummy_voltage_error = 2                     # Dummy variable for the voltage value error
 
 config = ParameterConfig()
 
@@ -24,8 +27,10 @@ if datatype == "folder":
     for file_name in files:
         full_file_path = os.path.join(config.input, file_name)
         tuples_list = load_data(full_file_path)
-
         voltage, height, micromotion = extract_data(tuples_list)
+        for i in range(len(voltage)):
+            voltage[i] = ufloat(voltage[i], config.dummy_voltage_error)
+            height[i] = ufloat(height[i], config.dummy_height_error)
 if datatype == "file":
     config.file_to_print = config.input
     with open(config.input, 'r') as file:
