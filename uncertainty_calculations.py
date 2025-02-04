4670da73b574c19483f379417faee95532970722
cole0wolf <colewolf@uw.edu>
2025-02-04 10:31:33 -0800

    Added multiple height uncertainties and implemented manufacturer specified voltage uncertainty

--- uncertainty_calculations.py
+++ uncertainty_calculations.py
@@ -6,8 +6,10 @@ class ParameterConfig:
     def __init__(self):
         self.input = "data/raw_micromotion"              # File or directory to be analyzed
         self.file_to_print = "8-18_Trial18_data.txt"     # Specific trial to print data from
-        self.dummy_height_error = 0.2                    # Dummy variable for the height value error
-        self.dummy_voltage_error = 2                     # Dummy variable for the voltage value error
+        self.dummy_height_error_1 = 0.2                  # Dummy variable for the height value error for first data set (8/8)
+        self.dummy_height_error_2 = 0.15                 # Dummy variable for the height value error for second data set (8/16)
+        self.dummy_height_error_3 = 0.12                 # Dummy variable for the height value error for third data set (8/18)
+        self.dummy_voltage_error = 0.0005                # Dummy variable for the voltage value error (0.05% as given by website)
 
 config = ParameterConfig()
 
@@ -29,12 +31,20 @@ if datatype == "folder":
         tuples_list = load_data(full_file_path)
         voltage, height, micromotion = extract_data(tuples_list)
         for i in range(len(voltage)):
-            voltage[i] = ufloat(voltage[i], config.dummy_voltage_error)
-            height[i] = ufloat(height[i], config.dummy_height_error)
+            voltage[i] = ufloat(voltage[i], (config.dummy_voltage_error * voltage[i]))
+            if i<2:
+                height[i] = ufloat(height[i], config.dummy_height_error_1)
+            if 8>i>=2:
+                height[i] = ufloat(height[i], config.dummy_height_error_2)
+            if i>=8:
+                height[i] = ufloat(height[i], config.dummy_height_error_3)
 if datatype == "file":
     config.file_to_print = config.input
     with open(config.input, 'r') as file:
         tuples_list = load_data(config.input)
         voltage, height, micromotion = extract_data(tuples_list)
+        for i in range(len(voltage)):
+            voltage[i] = ufloat(voltage[i], config.dummy_voltage_error)
+            height[i] = ufloat(height[i], config.dummy_height_error)
 
-print(height)
+print(voltage)
